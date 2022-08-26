5ff703c460c2ed4e9a61187ed0eb16ee3713aa9c
kami <mami@iqera.com>
2022-08-26 17:34:25 +0400

    My Project Setup completed successfully

---
+++ ShoppingBackend/src/main/java/net/chrisnet/App.java
@@ -0,0 +1,13 @@
+package net.chrisnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
