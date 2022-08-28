c2e3edb8bf37a33eff93012fd67c7e6296034943
kami <mami@iqera.com>
2022-08-28 12:09:14 +0400

    Completed the interface themes and other issues

--- OnlineShopping/src/main/java/net/chrisnet/onlineshopping/controller/PageController.java
+++ OnlineShopping/src/main/java/net/chrisnet/onlineshopping/controller/PageController.java
@@ -1,9 +1,7 @@
 package net.chrisnet.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller   
@@ -13,23 +11,29 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
-	@RequestMapping(value = {"/test"})
-	public ModelAndView index2(@RequestParam(value = "msg", required = false) String msg) {
-		if(msg == null) msg = "Hello World";
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", msg);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+	
+	
+	
 
-	@RequestMapping(value = {"/{name}"})
-	public ModelAndView index3(@PathVariable(value = "name", required = false) String name) {
-		if(name == null) name = "Hello World";
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", name);
-		return mv;
-	}
 }
